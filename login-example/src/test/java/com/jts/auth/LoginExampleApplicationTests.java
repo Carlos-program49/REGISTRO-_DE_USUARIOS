17ebdeda65176cd67823345e3a8235c635b0a67c
Carlos <carlos25314255@gmail.com>
2025-06-14 21:43:04 +0200

    AQUI SOLO AGREGUE UN COMENTARIO AQUI

--- login-example/src/test/java/com/jts/auth/LoginExampleApplicationTests.java
+++ login-example/src/test/java/com/jts/auth/LoginExampleApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoginExampleApplicationTests {
-//ESTE ES MI ULTIMO COMIT
+//ESTE ES MI ULTIMO COMIT FINA
 	@Test
 	void contextLoads() {
 	}
