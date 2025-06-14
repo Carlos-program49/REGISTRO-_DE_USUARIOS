0c78898ca2cc3c91a4119cbd4afbeebb832d63b4
Carlos <carlos25314255@gmail.com>
2025-06-14 21:39:58 +0200

    AQUI SOLO AGREGUE UN COMENTARIO

--- login-example/src/test/java/com/jts/auth/LoginExampleApplicationTests.java
+++ login-example/src/test/java/com/jts/auth/LoginExampleApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoginExampleApplicationTests {
-//CLASE PRINCIPAL DE GIT
+//ESTE ES MI ULTIMO COMIT
 	@Test
 	void contextLoads() {
 	}
